f2ad4eeaa185949e56be25a75fbc2a318a507627
Akshay <akshaybondre350@gmail.com>
2025-03-06 17:26:17 +0530

    Removed unnecessary files

--- src/app/dashboard/apparitions/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function ApparitionsPage() {
-    return (
-      <div className="p-6">
-        <h1 className="text-xl font-bold">Apparitions Page</h1>
-        <p>This is the Apparitions page content.</p>
-      </div>
-    );
-  }
-  
